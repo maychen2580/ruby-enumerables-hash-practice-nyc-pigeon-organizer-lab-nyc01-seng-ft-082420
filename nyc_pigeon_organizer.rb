a746a8331dfa05277a2e530fef032bd58603dd32
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-16 21:14:20 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_hash = {}
+  data.each do |key, value|
+  value.each do |inner_value, names|
+    names.each do |name|
+  if !new_hash[name]
+    new_hash[name] = {}
+  end
+  if !new_hash[name][key] 
+    new_hash[name][key] = []
+  end
+  new_hash[name][key] << inner_value.to_s
+  end
+  end
+ end
+ new_hash
 end
