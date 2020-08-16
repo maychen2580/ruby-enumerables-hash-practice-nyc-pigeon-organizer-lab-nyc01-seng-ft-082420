c6ffb53bc1dde1f86a290386426c4b2d28e04151
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-16 21:05:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,9 +3,10 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   new_hash = {}
-  data.each do|key, value|
+  data.each do |key, value|
+  value.each do |
   binding.pry
  
  
- 
+ end
 end
